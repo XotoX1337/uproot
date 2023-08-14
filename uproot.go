b49db64a8a2d3fd903a2ca5dc813b5a1ddf12dcc
frederic.leist <frederic.leist@dosing-gmbh.de>
2023-08-14 11:04:43 +0200

    restructured package uproot
    * added comments
    * changed tempDirName to os.Tempdir() + "uproot-fs"
    * added function CopyTo
    * added function RemoveTmp

--- uproot.go
+++ uproot.go
@@ -1,12 +1,13 @@
+// Package uproot provides simplified access to files that are embedded in an embed.FS.
+//
+// No support for files which are ambedded as []byte or string.
 package uproot
 
 import (
 	"embed"
 	"io/fs"
-	"log"
 	"os"
 	"path/filepath"
-	"runtime/debug"
 )
 
 type Uproot interface {
@@ -14,26 +15,69 @@ type Uproot interface {
 	TmpDir() string
 	Files() []string
 }
+
 type uproot struct {
 	eFS    *embed.FS
 	files  []string
 	tmpDir string
 }
 
-func New(eFS *embed.FS) *uproot {
-	uproot := &uproot{
-		eFS: eFS,
-	}
+const tmpDirName string = "uproot-fs"
 
-	err := uproot.scanFS()
+/*Exported functions*/
+
+// Creates a new instance of uproot
+func New(eFS *embed.FS) (*uproot, error) {
+	u := &uproot{
+		eFS:    eFS,
+		tmpDir: filepath.Join(os.TempDir(), tmpDirName),
+	}
+	err := u.scanFS()
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
-	uproot.setTmpDir()
-
-	return uproot
+	return u, nil
 }
 
+// Copies all files to the given directory.
+//
+// If the directory does not exist it will be created
+func (u *uproot) CopyTo(dir string) error {
+	return u.copy(dir)
+}
+
+// Copies all files of the file system to the temp directory
+func (u *uproot) CopyToTmp() error {
+	return u.copy(u.tmpDir)
+}
+
+// Deletes the temp directory including the contents
+func (u *uproot) RemoveTmp() error {
+	err := os.RemoveAll(u.tmpDir)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Returns the path to the temp directory
+func (u *uproot) TmpDir() string {
+	return u.tmpDir
+}
+
+// Returns the pointer to embedded FS
+func (u *uproot) FS() *embed.FS {
+	return u.eFS
+}
+
+// Returns the scanned files
+func (u *uproot) Files() []string {
+	return u.files
+}
+
+/*Unexported functions*/
+
+// Scans the directory and adds all files in uproot.files
 func (u *uproot) scanFS() error {
 	err := fs.WalkDir(u.eFS, ".", func(path string, d fs.DirEntry, err error) error {
 		if d.IsDir() {
@@ -46,39 +90,21 @@ func (u *uproot) scanFS() error {
 	return err
 }
 
-func (u *uproot) CopyToTmpDir() bool {
-
+func (u *uproot) copy(dir string) error {
 	for _, file := range u.files {
 		content, err := u.eFS.ReadFile(file)
 		if err != nil {
-			log.Fatalln(err)
+			return err
 		}
-
-		filename := filepath.Join(u.tmpDir, file)
-		os.MkdirAll(filepath.Dir(filename), 0666)
-		if err := os.WriteFile(filename, content, 0666); err != nil {
-			log.Fatalln(err)
+		path := filepath.Join(dir, filepath.Dir(file))
+		filepath := filepath.Join(path, filepath.Base(file))
+		err = os.MkdirAll(path, 0777)
+		if err != nil {
+			return err
+		}
+		if err := os.WriteFile(filepath, content, 0777); err != nil {
+			return err
 		}
 	}
-
-	return true
-}
-
-func (u *uproot) setTmpDir() {
-	bi, ok := debug.ReadBuildInfo()
-	if !ok {
-		log.Printf("Failed to read build info")
-
-	}
-	u.tmpDir = filepath.Join(os.TempDir(), bi.Main.Path)
-}
-
-func (u *uproot) GetTmpDir() string {
-	return u.tmpDir
-}
-func (u *uproot) GetFS() *embed.FS {
-	return u.eFS
-}
-func (u *uproot) GetFiles() []string {
-	return u.files
+	return nil
 }
